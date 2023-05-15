8139255da68e4207cc8a9620b1880a3b78b760d8
iagofeelix <93677121+iagofeelix@users.noreply.github.com>
2023-05-15 13:08:49 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   payloadKey: '57b0e476-142c-4762-8547-140b65cb1e9c',
   apis: {
-    imoveistock: 'https://imoveistockapi.tgtdigital.io/',
+    imoveistock: 'https://imoveistock-prod.tgtdigital.io/',
     // imoveistock: 'http://localhost:3000/',
   },
   redirect: {
